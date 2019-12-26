7908363ef2f1c65f8fb5839d145a34f68e2f850c
Dhruv Chitkara <dhruvchitkara27@gmail.com>
2019-12-27 02:09:47 +0530

    Final changes

--- browser-youtube/src/components/App.js
+++ browser-youtube/src/components/App.js
@@ -7,6 +7,10 @@ import VideoDetail from './VideoDetail';
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
 
+    componentDidMount() {
+        this.onTermSubmit('Perfect Edsheeran');
+    }
+
     onTermSubmit = async term => {
         const response = await youtube.get('/search', {
             params: {
@@ -14,22 +18,34 @@ class App extends React.Component {
             }
         });
 
-        this.setState({ videos: response.data.items });
+        this.setState({
+             videos: response.data.items,
+             selectedVideo: response.data.items[0]
+        });
     };
 
     onVideoSelect = (video) => {
-        
+        this.setState({ selectedVideo: video });
     };
 
     render() {
-        return(
-            <div className="ui container">
+        return (
+            <div className="dark ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
-            </div>
-        );
-    }
-}
-
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                            </div>
+                            <div className="five wide column">
+                                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                );
+            }
+        }
+        
 export default App;
